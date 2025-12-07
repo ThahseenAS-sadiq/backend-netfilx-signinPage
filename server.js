d3642b6b91a4dae2dd7e02ac040b7e8687b0591d
thahseen.sadiqas@gmail.com <thahseen.sadiqas@gmail.com>
2025-12-07 23:18:06 +0530

    import

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
-import { connectDB } from './config/db.js';
+import authRoutes from './Backend/routes/authRoutes.js';
+import { connectDB } from './Backend/config/db.js';
 
 dotenv.config();
 
